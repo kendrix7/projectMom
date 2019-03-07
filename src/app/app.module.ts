75f8f1d85329548eb14516622d1545edb605224b
Mark Nielsen <marknielsen7@outlook.com>
2019-03-07 00:27:45 -0700

    made a header component and some view components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeViewComponent } from './views/home-view/home-view.component';
+import { TimelineViewComponent } from './views/timeline-view/timeline-view.component';
+import { GalleryViewComponent } from './views/gallery-view/gallery-view.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeViewComponent,
+    TimelineViewComponent,
+    GalleryViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
